dbf55799129ca38df4fcddf541c9d5e63cac88e1
isp41 <isp41@LAB17-16>
2026-02-12 13:03:45 +0300

    Добавьте файлы проекта.

---
+++ Zadanie1ClientAgentV1/ModelsDb/Agent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Zadanie1ClientAgentV1.ModelsDb;
+
+public partial class Agent
+{
+    public int Id { get; set; }
+
+    public string FirstName { get; set; } = null!;
+
+    public string MiddleName { get; set; } = null!;
+
+    public string LastName { get; set; } = null!;
+
+    public int? DealShare { get; set; }
+}
---
+++ Zadanie1ClientAgentV1/ModelsDb/Zadanieee1Context.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Zadanie1ClientAgentV1.ModelsDb;
+
+public partial class Zadanieee1Context : DbContext
+{
+    public Zadanieee1Context()
+    {
+    }
+
+    public Zadanieee1Context(DbContextOptions<Zadanieee1Context> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Agent> Agents { get; set; }
+
+    public virtual DbSet<Client> Clients { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=192.168.24.15; Database=Zadanieee1; User=user10; Password=90513; Encrypt=false");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Agent>(entity =>
+        {
+            entity.Property(e => e.FirstName).HasMaxLength(50);
+            entity.Property(e => e.LastName).HasMaxLength(50);
+            entity.Property(e => e.MiddleName).HasMaxLength(50);
+        });
+
+        modelBuilder.Entity<Client>(entity =>
+        {
+            entity.ToTable("Client");
+
+            entity.Property(e => e.Email).HasMaxLength(255);
+            entity.Property(e => e.FirstName).HasMaxLength(255);
+            entity.Property(e => e.LastName).HasMaxLength(255);
+            entity.Property(e => e.MiddleName).HasMaxLength(255);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
